4303d9ce6079b23511333bca245b7179673e5c23
oghenenyerhovwo <93879454+oghenenyerhovwo@users.noreply.github.com>
2022-01-12 17:54:29 +0100

    make it better

--- connections/storageConnection.js
+++ connections/storageConnection.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv"
 dotenv.config()
 
 const storage = multer.diskStorage({})
-
+console.log(process.env.MONGODB_URL)
 export const upload= multer({storage})
 
 export function  cloud_keys (){
