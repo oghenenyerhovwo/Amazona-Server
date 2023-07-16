49277607bb2527dd033a5d0f2b9c9eed1f568ec2
oghenenyerhovwo <93879454+oghenenyerhovwo@users.noreply.github.com>
2023-07-16 15:46:03 +0100

    Update userRouter.js

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -83,7 +83,7 @@ router.post("/signIn",
                         name: foundUser.name,
                         email: foundUser.email,
                         isAdmin: foundUser.isAdmin,
-                        isSeller: foundUser.isSeller || true,
+                        isSeller: foundUser.isSeller,
                         token: generateToken(foundUser)
                     })
                     return;
